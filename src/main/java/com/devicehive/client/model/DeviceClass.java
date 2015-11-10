1770d3585f9de5f48273f135da517ca5379635fe
Eugene <edubovik90@gmail.com>
2015-11-10 12:49:07 +0200

    Refactoring, DeviceExample tests

--- src/main/java/com/devicehive/client/model/DeviceClass.java
+++ src/main/java/com/devicehive/client/model/DeviceClass.java
@@ -1,5 +1,7 @@
 package com.devicehive.client.model;
 
+import com.devicehive.client.model.Equipment;
+import com.devicehive.client.model.JsonStringWrapper;
 import com.google.gson.annotations.SerializedName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
