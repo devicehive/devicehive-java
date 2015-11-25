c183e43c507e182fd2aa9d5d70f57f9aba9d79e9
Eugene <edubovik90@gmail.com>
2015-11-25 17:25:31 +0200

    deleted unnecessary items

--- src/main/java/examples/Const.java
+++ src/main/java/examples/Const.java
@@ -25,4 +25,35 @@ public class Const {
     public static final DeviceCommandWrapper TURN_ON = new DeviceCommandWrapper();
     public static final DeviceCommandWrapper TURN_OFF = new DeviceCommandWrapper();
 
+
+    /**
+     * Application name
+     */
+    public static final String APP_NAME = "Examples set";
+
+    /**
+     * If unknown command found or command was used incorrect this message displayed.
+     */
+    public static final String PARSE_EXCEPTION_MESSAGE = "Unknown command!";
+
+    /**
+     * This command identifies that the websocket protocol should be preferred
+     */
+    public static final String USE_SOCKETS = "useSockets";
+
+    /**
+     * Description of useSockets command
+     */
+    public static final String USE_SOCKETS_DESCRIPTION = "If used then prefer the websocket protocol";
+
+    /**
+     * This command line parameter stands for server URL.
+     */
+//    public static final String URL = "http://playground.devicehive.com/api/rest";
+
+    /**
+     * Description of url option.
+     */
+    public static final String URL_DESCRIPTION = "REST service url";
+
 }
