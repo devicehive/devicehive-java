8b4dd69bd62d4a95d167e5a37ecd111ddea8f47f
Eugene <edubovik90@gmail.com>
2015-12-11 17:27:31 +0200

    Updated tyrus ver. to enable websockets

---
+++ src/main/java/com/devicehive/client/websocket/HiveFactory.java
@@ -0,0 +1,47 @@
+package com.devicehive.client.websocket;
+
+import com.devicehive.client.websocket.api.HiveClient;
+import com.devicehive.client.websocket.api.impl.HiveClientRestImpl;
+import com.devicehive.client.websocket.api.impl.HiveClientWebsocketImpl;
+import com.devicehive.client.websocket.context.RestAgent;
+import com.devicehive.client.websocket.context.WebsocketAgent;
+import com.devicehive.client.websocket.model.exceptions.HiveException;
+
+import java.net.URI;
+
+public final class HiveFactory {
+
+    private HiveFactory() {
+    }
+
+
+    /**
+     * Creates an instance of {@link HiveClient} connected to the Device Hive server.
+     *
+     * @param restUri          the Device Hive server RESTful API URI
+     * @param preferWebsockets if set to {@code true}, the client will attempt to use websockets for communication with
+     *                         the server.
+     * @return an instance of {@link HiveClient}
+     * @throws HiveException if a connection error occurs
+     */
+    public static HiveClient createClient(URI restUri, boolean preferWebsockets) throws HiveException {
+        if (preferWebsockets) {
+            return new HiveClientWebsocketImpl(createWebsocketClientAgent(restUri));
+        } else {
+            return new HiveClientRestImpl(createRestAgent(restUri));
+        }
+    }
+
+    private static RestAgent createRestAgent(URI restUri) throws HiveException {
+        RestAgent agent = new RestAgent(restUri);
+        agent.connect();
+        return agent;
+    }
+
+    private static WebsocketAgent createWebsocketClientAgent(URI restUri) throws HiveException {
+        WebsocketAgent agent = new WebsocketAgent(restUri);
+        agent.connect();
+        return agent;
+    }
+
+}
