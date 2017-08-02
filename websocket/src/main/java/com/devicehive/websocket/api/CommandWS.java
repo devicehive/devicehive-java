61417f1491ef01af7f1f3e56616add87a35116fd
Eugene Dubovik <edubovik90@gmail.com>
2017-08-02 13:16:56 +0300

    Feature: WS WIP; Added Notification; Fixed REST and WS models;

--- websocket/src/main/java/com/devicehive/websocket/api/CommandWS.java
+++ websocket/src/main/java/com/devicehive/websocket/api/CommandWS.java
@@ -1,12 +1,14 @@
 package com.devicehive.websocket.api;
 
 import com.devicehive.websocket.listener.CommandListener;
+import com.devicehive.websocket.model.SortOrder;
 import com.devicehive.websocket.model.request.*;
 import com.devicehive.websocket.model.request.data.DeviceCommandWrapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import org.joda.time.DateTime;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 public class CommandWS extends BaseWebSocketListener implements CommandApi {
@@ -33,7 +35,7 @@ public class CommandWS extends BaseWebSocketListener implements CommandApi {
     }
 
     @Override
-    public void get(String deviceId, Long commandId, Long requestId) {
+    public void get(@Nullable Long requestId, String deviceId, Long commandId) {
         DeviceGetAction action = new DeviceGetAction();
         action.setDeviceId(deviceId);
         action.setCommandId(commandId);
@@ -42,7 +44,7 @@ public class CommandWS extends BaseWebSocketListener implements CommandApi {
     }
 
     @Override
-    public void list(String deviceId, Long requestId, DateTime start, DateTime end, String commandName, String status, CommandListAction.SortOrder sortOrder, Integer take, Integer skip) {
+    public void list(@Nullable Long requestId, String deviceId, DateTime start, DateTime end, String commandName, String status, SortOrder sortOrder, Integer take, Integer skip) {
         CommandListAction action = new CommandListAction();
         action.setDeviceId(deviceId);
         action.setStart(start);
@@ -57,7 +59,7 @@ public class CommandWS extends BaseWebSocketListener implements CommandApi {
     }
 
     @Override
-    public void insert(String deviceId, Long requestId, DeviceCommandWrapper wrapper) {
+    public void insert(@Nullable Long requestId, String deviceId, DeviceCommandWrapper wrapper) {
         CommandInsertAction action = new CommandInsertAction();
         action.setCommand(wrapper);
         action.setDeviceId(deviceId);
@@ -66,7 +68,7 @@ public class CommandWS extends BaseWebSocketListener implements CommandApi {
     }
 
     @Override
-    public void update(String deviceId, String commandId, Long requestId, DeviceCommandWrapper wrapper) {
+    public void update(@Nullable Long requestId, String deviceId, String commandId, DeviceCommandWrapper wrapper) {
         CommandUpdateAction action = new CommandUpdateAction();
         action.setDeviceId(deviceId);
         action.setCommandId(commandId);
@@ -76,7 +78,7 @@ public class CommandWS extends BaseWebSocketListener implements CommandApi {
     }
 
     @Override
-    public void subscribe(List<String> names, String deviceId, Long requestId, List<String> deviceIds, DateTime timestamp, Integer limit) {
+    public void subscribe(@Nullable Long requestId, List<String> names, String deviceId, List<String> deviceIds, DateTime timestamp, Integer limit) {
         CommandSubscribeAction action = new CommandSubscribeAction();
         action.setNames(names);
         action.setDeviceId(deviceId);
@@ -88,7 +90,7 @@ public class CommandWS extends BaseWebSocketListener implements CommandApi {
     }
 
     @Override
-    public void unsubscribe(String subscriptionId, Long requestId, List<String> deviceIds) {
+    public void unsubscribe(@Nullable Long requestId, String subscriptionId, List<String> deviceIds) {
         CommandUnsubscribeAction action = new CommandUnsubscribeAction();
         action.setSubscriptionId(subscriptionId);
         action.setDeviceIds(deviceIds);
