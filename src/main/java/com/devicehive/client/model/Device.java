ec6df0b858cbc31064eac7b95be18cb2d9298f6b
Eugene Dubovik <edubovik90@gmail.com>
2017-09-28 17:38:20 +0300

    Feature: Device Entity; https://support.dataart.com/browse/DEV-177 ; Fixed Device's code generation;

--- src/main/java/com/devicehive/client/model/Device.java
+++ src/main/java/com/devicehive/client/model/Device.java
@@ -2,13 +2,25 @@ package com.devicehive.client.model;
 
 import com.devicehive.client.DeviceHive;
 import com.devicehive.rest.model.JsonStringWrapper;
+import lombok.Data;
 import org.joda.time.DateTime;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.util.List;
-
+@Data
 public class Device implements DeviceInterface {
+
+
+    private String id = null;
+
+    private String name = null;
+
+    private JsonStringWrapper data = null;
+
+    private Long networkId = null;
+
+    private Boolean isBlocked = false;
 
     private Device() {
     }
@@ -29,58 +41,6 @@ public class Device implements DeviceInterface {
         result.networkId = device.getNetworkId();
         result.isBlocked = device.getIsBlocked();
         return result;
-    }
-
-    private String id = null;
-
-    private String name = null;
-
-    private JsonStringWrapper data = null;
-
-    private Long networkId = null;
-
-    private Boolean isBlocked = false;
-
-    private DeviceHive deviceHive;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public JsonStringWrapper getData() {
-        return data;
-    }
-
-    public void setData(JsonStringWrapper data) {
-        this.data = data;
-    }
-
-    public Long getNetworkId() {
-        return networkId;
-    }
-
-    public void setNetworkId(Long networkId) {
-        this.networkId = networkId;
-    }
-
-    public Boolean getBlocked() {
-        return isBlocked;
-    }
-
-    public void setBlocked(Boolean blocked) {
-        isBlocked = blocked;
     }
 
     public void save() throws IOException {
