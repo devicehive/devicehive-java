ac7ee13062ea1fff1d95fc87d54ee96e42523cff
Eugene Dubovik <edubovik90@gmail.com>
2017-10-10 11:23:32 +0300

    Updated tests;

--- src/test/DeviceTest.java
+++ src/test/DeviceTest.java
@@ -19,12 +19,12 @@ public class DeviceTest {
 
     private static final String URL = "http://playground.dev.devicehive.com/api/rest/";
     private static final String WS_URL = "ws://playground.dev.devicehive.com/api/websocket";
-    public static final String NOTIFICATION_A = "notificationA";
-    public static final String NOTIFICATION_B = "notificationB";
-    public static final String NOTIFICATION_Z = "notificationZ";
-    public static final String COM_A = "comA";
-    public static final String COM_B = "comB";
-    public static final String COM_Z = "comZ";
+    private static final String NOTIFICATION_A = "notificationA";
+    private static final String NOTIFICATION_B = "notificationB";
+    private static final String NOTIFICATION_Z = "notificationZ";
+    private static final String COM_A = "comA";
+    private static final String COM_B = "comB";
+    private static final String COM_Z = "comZ";
     private String accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjp7InVzZXJJZCI6MSwiYWN0aW9ucyI6WyIqIl0sIm5ldHdvcmtJZHMiOlsiKiJdLCJkZXZpY2VJZHMiOlsiKiJdLCJleHBpcmF0aW9uIjoxNTM2OTI1MTA2NDM1LCJ0b2tlblR5cGUiOiJBQ0NFU1MifX0.DVRKVgrtnv35MWwxR1T8bLm83-RJCfloYuoEjvYPQ4s";
     private String refreshToken = "eyJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjp7InVzZXJJZCI6MSwiYWN0aW9ucyI6WyIqIl0sIm5ldHdvcmtJZHMiOlsiKiJdLCJkZXZpY2VJZHMiOlsiKiJdLCJleHBpcmF0aW9uIjoxNTM2OTI1MTA2NDM1LCJ0b2tlblR5cGUiOiJSRUZSRVNIIn19.7alYTD5kb_imglE7NyRhjQBFqXhqpfJJs-ZA68yJZiQ";
 
