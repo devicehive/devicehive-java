9d027807194fbb99c57f460131650055a8e1b64a
Andrey Latysh <alatysh@dataart.com>
2017-11-03 12:46:03 +0200

    DEV-271; https://support.dataart.com/browse/DEV-271 Temporary after merge build.

--- websocket/src/test/java/com/github/devicehive/websocket/Helper.java
+++ websocket/src/test/java/com/github/devicehive/websocket/Helper.java
@@ -6,6 +6,7 @@ import com.github.devicehive.websocket.api.*;
 import com.github.devicehive.websocket.listener.ConfigurationListener;
 import com.github.devicehive.websocket.listener.DeviceListener;
 import com.github.devicehive.websocket.listener.TokenListener;
+import com.github.devicehive.websocket.listener.UserListener;
 import com.github.devicehive.websocket.model.repsonse.*;
 
 import java.util.List;
@@ -66,10 +67,10 @@ class Helper {
                 }
         );
         client.authenticate(ACCESS_TOKEN);
-        latch.await(1, TimeUnit.SECONDS);
+        latch.await(awaitTimeout, awaitTimeUnit);
     }
 
-    boolean deleteConfigurations(String name) throws InterruptedException {
+    boolean deleteConfiguration(String name) throws InterruptedException {
         final CountDownLatch latch = new CountDownLatch(1);
 
         final Counter counter = new Counter();
@@ -151,6 +152,79 @@ class Helper {
 
     void deleteDevice(String id) {
         deviceWS.delete(null, id);
+    }
+
+    boolean deleteUser(Long id) throws InterruptedException {
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        authenticate();
+
+        final Counter counter = new Counter();
+        final UserWS userWS = client.createUserWS();
+        userWS.setListener(new UserListener() {
+            @Override
+            public void onList(UserListResponse response) {
+
+            }
+
+            @Override
+            public void onGet(UserGetResponse response) {
+
+            }
+
+            @Override
+            public void onInsert(UserInsertResponse response) {
+
+            }
+
+            @Override
+            public void onUpdate(ResponseAction response) {
+
+            }
+
+            @Override
+            public void onDelete(ResponseAction response) {
+                if (response.getStatus().equals(ResponseAction.SUCCESS)) {
+                    counter.increment();
+                }
+                latch.countDown();
+            }
+
+            @Override
+            public void onGetCurrent(UserGetCurrentResponse response) {
+
+            }
+
+            @Override
+            public void onUpdateCurrent(ResponseAction response) {
+
+            }
+
+            @Override
+            public void onGetNetwork(UserGetNetworkResponse response) {
+
+            }
+
+            @Override
+            public void onAssignNetwork(ResponseAction response) {
+
+            }
+
+            @Override
+            public void onUnassignNetwork(ResponseAction response) {
+
+            }
+
+            @Override
+            public void onError(ErrorResponse error) {
+
+            }
+
+        });
+
+        userWS.delete(null, id);
+        latch.await(awaitTimeout, awaitTimeUnit);
+        return counter.getCount() == 1;
     }
 
 }
