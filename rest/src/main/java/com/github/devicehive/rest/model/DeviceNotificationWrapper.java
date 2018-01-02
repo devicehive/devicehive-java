b2d884b565ffe975f2d95fd7d125c3c8e39facce
Eugene Dubovik <edubovik90@gmail.com>
2018-01-02 13:10:29 +0200

    Updated copyright;

--- rest/src/main/java/com/github/devicehive/rest/model/DeviceNotificationWrapper.java
+++ rest/src/main/java/com/github/devicehive/rest/model/DeviceNotificationWrapper.java
@@ -3,7 +3,7 @@
  *
  *   DeviceNotificationWrapper.java
  *
- *   Copyright (C) 2017 DataArt
+ *   Copyright (C) 2018 DataArt
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
